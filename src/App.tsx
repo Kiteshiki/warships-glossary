97abd9be07e14e63321cc4036967cd1adfc2f0bb
Idris.S <isalavatov-ext@ifeelsmart.com>
2022-09-15 23:54:51 +0300

    chore: init repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="bg-black h-screen relative text-white p-4">
+            <h1 className="my-4 text-4xl text-center block">Warships Glossary</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
