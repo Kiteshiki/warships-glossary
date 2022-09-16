3e4bbe3fd3ab1421744986097c3c03ef3c52a559
Idris.S <isalavatov-ext@ifeelsmart.com>
2022-09-16 20:21:32 +0300

    chore: update Tile stylings

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,3 +1,4 @@
+import { convertNumberToRoman } from 'helpers'
 import { Vehicle } from 'typings/vehicle'
 
 interface TileProps {
@@ -6,15 +7,43 @@ interface TileProps {
 
 export const Tile = ({ entity }: TileProps) => {
     return (
-        <div className="shadow rounded-md p-4 max-w-sm w-full mx-auto bg-gray-900">
+        <div className="shadow rounded-md p-4 max-w-lg w-full mx-auto bg-gray-900">
             <div className="flex flex-wrap">
-                <div className="w-full aspect-[109/64]">
-                    <img src={entity.icons.medium} alt={entity.title} loading="lazy" />
+                <div className="w-full aspect-[435/256] relative">
+                    <img
+                        className="mx-auto"
+                        src={entity.icons.medium}
+                        alt={entity.title}
+                        loading="lazy"
+                    />
+
+                    <div className="absolute top-0 right-0">
+                        <img
+                            width={52}
+                            height={32}
+                            src={entity.nation.icons.small}
+                            alt={entity.nation.title}
+                            loading="lazy"
+                        />
+                    </div>
                 </div>
                 <div className="flex-1 space-y-2 pt-4">
-                    <div className="text-lg">{entity.title}</div>
+                    <div className="flex justify-between">
+                        <div className="text-xl">{entity.title}</div>
+                        <div className="font-light whitespace-nowrap text-cyan-300">
+                            Tier {convertNumberToRoman(entity.level)}
+                        </div>
+                    </div>
+                    <div className="font-thin">
+                        {entity.type.title}
+                        <img
+                            className="inline-block ml-2"
+                            src={entity.type.icons.default}
+                            alt={entity.type.title}
+                        />
+                    </div>
                     <div
-                        className="font-light text-gray-300"
+                        className="font-light text-gray-400"
                         dangerouslySetInnerHTML={{ __html: entity.description }}
                     />
                 </div>
